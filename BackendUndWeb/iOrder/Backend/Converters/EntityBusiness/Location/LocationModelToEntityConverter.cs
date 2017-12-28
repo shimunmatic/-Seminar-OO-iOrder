3c809e3fcf2d8c926085581aa872e4a5f8a1b977
shimunmatic <shimun.matic@gmail.com>
2017-12-28 22:19:14 +0100

    Added new repos implementations, business models and converters

---
+++ BackendUndWeb/iOrder/Backend/Converters/EntityBusiness/Location/LocationModelToEntityConverter.cs
@@ -0,0 +1,39 @@
+﻿using Backend.Models.Business;
+using Backend.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Converters.EntityBusiness
+{
+    public class LocationModelToEntityConverter : IConverter<Location, LocationEntity>
+    {
+        public LocationEntity Convert(Location Source)
+        {
+            if (null == Source) return null;
+            return new LocationEntity
+            {
+                Id = Source.Id,
+                EstablishmentId = Source.EstablishmentId,
+                Name = Source.Name
+            };
+        }
+
+        public IEnumerable<LocationEntity> Convert(IEnumerable<Location> Source)
+        {
+            if (null == Source) return null;
+            var locations = new List<LocationEntity>();
+            foreach (var l in Source)
+            {
+                locations.Add(new LocationEntity
+                {
+                    Id = l.Id,
+                    EstablishmentId = l.EstablishmentId,
+                    Name = l.Name
+                });
+            }
+            return locations;
+        }
+    }
+}
--- BackendUndWeb/iOrder/Backend/Models/Business/Location.cs
+++ BackendUndWeb/iOrder/Backend/Models/Business/Location.cs
@@ -7,5 +7,8 @@ namespace Backend.Models.Business
 {
     public class Location
     {
+        public virtual long Id { get; set; }
+        public virtual long EstablishmentId { get; set; }
+        public virtual string Name { get; set; }
     }
 }
